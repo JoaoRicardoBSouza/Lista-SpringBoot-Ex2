55a912de6cf1a1c7dd816c13b48a7923c0b99941
JoaoRicardoBSouza <joaoricardobsouza@gmail.com>
2026-03-18 12:50:53 -0300

    first commit

---
+++ src/test/java/com/ex2/exercicio2/Exercicio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex2.exercicio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercicio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
